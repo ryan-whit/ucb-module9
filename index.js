beb42632421d573994fc99b0a0d06534db162ded
Ryan Whitmore <ryan.whit@outlook.com>
2022-08-28 16:22:53 -0700

    Added package-lock file after installation, and added inquirerer command line questions.

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// README sections
+// title (in README title)
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+
+// TODO: specify the license choices
+const licenseOptions = ["option1", "option2"];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Please input the project of your title: ',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide the description of your project: ',
+    },
+    {
+      type: 'input',
+      name: 'installationInstructions',
+      message: 'Please provide installation instructions: ',
+    },
+    {
+      type: 'input',
+      name: 'usageInformation',
+      message: 'Please provide usageInformation: ',
+    },
+    {
+      type: 'input',
+      name: 'contributingGuidelines',
+      message: 'Please provide the contributing guidelines: ',
+    },
+    {
+      type: 'input',
+      name: 'testInstructions',
+      message: 'Please provide the test instructions: ',
+    },
+    {
+			type: 'list',
+			name: 'license',
+			message: 'Please select the license for your project: ',
+			choices: licenseOptions,
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Please provide your GitHub username: ',
+    },
+    {
+      type: 'input',
+      name: 'emailAddress',
+      message: 'Please provide your email address: ',
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
